27af2a917eaec03844422fb9da5115cd37308e09
ajeshh <ajeshshah@gmail.com>
2014-11-23 17:02:07 -0800

    reset button added

--- tictactoe.js
+++ tictactoe.js
@@ -3,9 +3,9 @@ var x = [];
 var o = [];
 var winCombo = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 var win = false;
+var reset = document.getElementById("reset");
 
 function divClicked(divNumber) {
-	
 	var playerTurn = document.getElementById("playerTurn");
 	var theDiv = "div" + divNumber;
 	var d = document.getElementById(theDiv);
@@ -23,16 +23,13 @@ function divClicked(divNumber) {
 	x.push(divNumber);
 	playerTurn.innerHTML = "O's turn";
 	}
-
 	else {d.innerHTML = "O";
 	d.style.backgroundColor = "blue";
 	d.style.color = "white";
 	o.push(divNumber);
 	playerTurn.innerHTML = "X's turn";
 	}
-
 	}
-
 	for (var i = 0; i < winCombo.length; i++) {
 		if(x.indexOf(winCombo[i][0]) >= 0) {
 			if(x.indexOf(winCombo[i][1]) >= 0) {
@@ -44,7 +41,6 @@ function divClicked(divNumber) {
 			}
 		}
 	}
-
 	for (i = 0; i < winCombo.length; i++) {
 		if(o.indexOf(winCombo[i][0]) >= 0) {
 			if(o.indexOf(winCombo[i][1]) >= 0) {
@@ -57,14 +53,12 @@ function divClicked(divNumber) {
 			}
 		}
 	}
-
 	if (turn === 9 && win === false){
 		alert("Both of y'all are terrible at this!");
 		location.reload();
 	}
-
-
-
 };
+
+
 
 
